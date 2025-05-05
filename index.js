ff5e7edd05dbcb0b436ec41cf2face98b7a18a99
Abhilasha Deshmukh <88450158+abhibdesh@users.noreply.github.com>
2025-05-05 10:25:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    // origin: "http://localhost:5173", 
+    origin: "https://matrimony-ui-mern.vercel.app/", 
     credentials: true, 
   })
 );
